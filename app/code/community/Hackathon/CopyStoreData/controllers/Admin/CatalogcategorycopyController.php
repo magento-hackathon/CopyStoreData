43f16b9d89426b9863d6199a9c5bf7c3623b090d
Jeroen Vermeulen <info@jeroenvermeulen.eu>
2015-05-29 12:09:29 +0200

    Form file for Category Copy is working

--- app/code/community/Hackathon/CopyStoreData/controllers/Admin/CatalogcategorycopyController.php
+++ app/code/community/Hackathon/CopyStoreData/controllers/Admin/CatalogcategorycopyController.php
@@ -3,20 +3,18 @@
 class Hackathon_CopyStoreData_Admin_CatalogcategorycopyController extends Mage_Adminhtml_Controller_Action
 {
 
-    // URL:  http://[MAGROOT]/admin/catalog_category_copy/index/key/###########/
-    // If "storecode in url" is enabled there is an extra "/admin" before "/jv_adminform"
+    // URL:  http://[MAGROOT]/admin/catalogcategorycopy/index/key/###########/
+    // If "storecode in url" is enabled there is an extra "/admin" before "/catalogcategorycopy"
     public function indexAction()
     {
-echo 'TEST TEST';
-
-//        $this->loadLayout();
-//        $this->renderLayout();
+        $this->loadLayout();
+        $this->renderLayout();
         // Layout will be chosen by Mage_Core_Controller_Varien_Action::addActionLayoutHandles
-        // Layout file:        /app/design/adminhtml/default/default/layout/jeroenvermeulen_adminform.xml
-        // Item in that file:  jv_adminform_demo_form_index
+        // Layout file:        /app/design/adminhtml/default/default/layout/hackathon/copystoredata.xml
+        // Item in that file:  adminhtml_catalogcategorycopy_index
 
         // To debug layout XML enable:
-        // header( 'Content-Type: text/xml' ); echo $this->getLayout()->getXmlString(); exit;
+        //header( 'Content-Type: text/xml' ); echo $this->getLayout()->getXmlString(); exit;
     }
 
 
